97948f2e0fe4edca648d8acb804d589dac0d70c8
unknown <brian_warner25@yahoo.com>
2018-05-01 12:24:56 -0700

    html and dom elem

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+var whenWins = 0;
+var remGuess = 10;
+var hidWord = ['vader', 'skywalker', 'kenobi', 'force', 'death star','tauntaun'];
+
+//DOM ELEMENTS
+
+var winSpan = document.getElementByClassName("whenWins");
+var wordSpan = document.getElementByClassName("hidWord");
+var remainSpan = document.getElementByClassName("remGuess");
+var userSpan = document.getElementByClassName("userGuess");
