81535c1c31b99d3f9aafa2de77277f756a6b7581
unknown <brian_warner25@yahoo.com>
2018-05-03 18:35:28 -0700

    added functions to manDOM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,70 @@
-var whenWins = 0;
-var remGuess = 10;
-var hidWord = ['vader', 'skywalker', 'kenobi', 'force', 'death star','tauntaun'];
+//variables
+
+// current word must be set before running this function
+function setCharAt(str,index,chr) {
+    if(index > str.length-1) return str;
+    return str.substr(0,index) + chr + str.substr(index+1);
+}
+var initUserGuess = function() {
+    console.log("asd")
+    userGuess = "";
+    for(var i = 0; i < currentWord.length; i++) {
+        userGuess += "_";
+    }
+}
+var updateHTML = function() {
+    console.log(userGuess)
+    winsSpan.textContent = wins;
+    remainSpan.textContent = remainingGuess;
+    wordSpan.textContent = userGuess;
+    userSpan.textContent = guessedWords;
+
+}
+var findUserGuess = function(key) {
+    var foundIndex = currentWord.indexOf(key)
+    if(foundIndex === -1) {
+        //not found!!!
+    }
+    else {
+        console.log("found");
+        setCharAt(userGuess, foundIndex, key);
+    }
+}
+var wins = 0;
+var remainingGuess = 10;
+var word = ['vader', 'skywalker', 'kenobi', 'force'];
+var userGuess = "";
+var guessedWords = [];
+
 
 //DOM ELEMENTS
 
-var winSpan = document.getElementByClassName("whenWins");
-var wordSpan = document.getElementByClassName("hidWord");
-var remainSpan = document.getElementByClassName("remGuess");
-var userSpan = document.getElementByClassName("userGuess");
+var winsSpan = document.getElementById("wins");
+var wordSpan = document.getElementById("word");
+var remainSpan = document.getElementById("remainingGuess");
+var userSpan = document.getElementById("userGuess");
+// var computerGuess = computerChoices["word"];
+
+//init section
+var currentWord = word[Math.floor(Math.random() * word.length)];
+initUserGuess();
+updateHTML();
+console.log(currentWord)
+document.onkeyup = function(event) {
+    
+    var currentLetter = event.key;
+    findUserGuess(currentLetter);
+    guessedWords.push(currentLetter);
+    updateHTML();
+    
+}
+
+
+
+
+
+
+
+
+
+
